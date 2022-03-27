127eddc9ce802c8a4cb7ab94c2838592bab9e4ff
Mahima Kaushal <30713765+MahimaKaushal@users.noreply.github.com>
2022-03-28 02:14:51 +0530

    CiscApplication file is updated

--- src/main/java/com/javatechi/cicd/CicdApplication.java
+++ src/main/java/com/javatechi/cicd/CicdApplication.java
@@ -2,10 +2,15 @@ package com.javatechi.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CicdApplication {
 
+	@GetMapping
+	public String hl() {
+		return "hi cicd application";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
 	}
